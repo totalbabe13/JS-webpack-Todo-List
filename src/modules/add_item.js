cbd71f025c3fb0dfdae20e32520f31f715fff7aa
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-10-30 16:44:01 -0500

    todo item now formatted when saved- working on css to look sharper

--- src/modules/add_item.js
+++ src/modules/add_item.js
@@ -14,99 +14,158 @@ const getUserInput = ((event) => {
    let priorityValue = document.querySelector('[name=priority-x]').dataset.priority;
    let notesValue = document.querySelector('[name=notes-x]').value;
    let userTodoItem = new TodoItem(descriptionValue, dateValue, priorityValue, notesValue)
-   //Remove Form
-   document.getElementById("todo-view").removeChild(document.getElementById("todo-item"));
-   console.log(userTodoItem);
+   //convert Form
+   //add checkMarkButton
+   formatSavedItem(userTodoItem);
    return userTodoItem;
-   //xlist.push(userTodoItem);
-})
-const createItemForm =(()=>{
-  let itemFormpPresent = document.getElementById("todo-item");
-  if (itemFormpPresent === null){
-  // list item container
-  let listView = document.getElementById("todo-view");
-  let addButton = document.getElementById("add-todo");
-  let itemContainer = document.createElement("form");
-  // itemContainer.className ="todo-item";
-  itemContainer.id ="todo-item";
-  itemContainer.addEventListener("submit",getUserInput);
-  listView.appendChild(itemContainer);
 
-  //check mark structure
+})
+
+const createItemForm = (()=>{
+  let itemFormpPresent = document.getElementById("todo-item");
+  //ONLY opens one Form at a time
+  if (itemFormpPresent === null){
+    // list item container
+    let listView = document.getElementById("todo-view");
+    let addButton = document.getElementById("add-todo");
+    let itemContainer = document.createElement("form");
+    // itemContainer.className ="todo-item";
+    itemContainer.id ="todo-item";
+    itemContainer.addEventListener("submit",getUserInput);
+    listView.appendChild(itemContainer);
+
+    //check mark structure
+    let checkMarkContainer = document.createElement("div");
+    checkMarkContainer.className ="check-mark-container";
+    itemContainer.appendChild(checkMarkContainer);
+
+    //check mark Button
+    let checkMarkButton = document.createElement("input");
+    checkMarkButton.setAttribute("type","Button");
+    checkMarkButton.setAttribute("name","completed-x")
+    checkMarkButton.className = "completed";
+    checkMarkContainer.appendChild(checkMarkButton);
+
+    //todo text description
+    let todoDescription = document.createElement("input");
+    todoDescription.className = "todo-content";
+    todoDescription.setAttribute("name","description-x");
+    todoDescription.setAttribute("placeholder","Type in description")
+    todoDescription.setAttribute("required","")
+    itemContainer.appendChild(todoDescription);
+
+    //todo date set
+    let date =document.createElement("input");
+    date.className = "todo-date";
+    date.setAttribute("type","date");
+    date.setAttribute("name","date-x");
+    date.setAttribute("required","")
+    itemContainer.appendChild(date);
+
+    //todo priority level(red, orange, yellow);
+    let priority = document.createElement("div");
+    priority.className = "todo-priority";
+    itemContainer.appendChild(priority);
+
+    let priorityToggle = document.createElement("input");
+    priorityToggle.setAttribute("type","button")
+    priorityToggle.setAttribute("name","priority-x");
+    priorityToggle.setAttribute("data-priority","normal")
+    priorityToggle.className = "todo-priority-set";
+    priorityToggle.onclick = changePriority;
+    priority.appendChild(priorityToggle);
+
+    // delete item Button
+    let deleteItem = document.createElement("div");
+    deleteItem.className = "todo-delete";
+    itemContainer.appendChild(deleteItem);
+
+    let deleteDiv = document.createElement("div");
+    deleteDiv.className = "delete-icon";
+    deleteItem.appendChild(deleteDiv);
+
+    let deleteButton = document.createElement("input");
+    deleteButton.className = "todo-remove";
+    deleteButton.onclick = cancelForm;
+    deleteButton.setAttribute("type","button");
+    deleteButton.setAttribute("value","+");
+    deleteDiv.appendChild(deleteButton);
+
+    //NOTES text field toggle button
+    let notes = document.createElement("input");
+    notes.className = "todo-notes";
+    notes.setAttribute("type","text");
+    notes.setAttribute('name', "notes-x");
+    notes.setAttribute("placeholder","Add Notes here :)");
+    // notes.onclick = addNotes;
+    itemContainer.appendChild(notes);
+
+    //SAVE button
+    let saveItem = document.createElement("input");
+    saveItem.className = "todo-save";
+    saveItem.setAttribute("type","submit");
+    saveItem.setAttribute("value","Save");
+    itemContainer.appendChild(saveItem);
+    // console.log("createItemForm has run")
+    } else {alert("You can only add One Item at a time!!");}
+})
+const formatSavedItem = ((userTodoItem) => {
+  //replace form with div
+  let currentform = document.getElementById("todo-item");
+  let newContainer = document.createElement('div');
+  newContainer.className = "todo-item"
+  currentform.parentNode.replaceChild(newContainer,currentform);
+
   let checkMarkContainer = document.createElement("div");
   checkMarkContainer.className ="check-mark-container";
-  itemContainer.appendChild(checkMarkContainer);
-
-  //check mark Button
-  let checkMarkButton = document.createElement("input");
-  checkMarkButton.setAttribute("type","Button");
-  checkMarkButton.setAttribute("name","completed-x")
+  newContainer.appendChild(checkMarkContainer);
+  let checkMarkButton = document.createElement("button");
   checkMarkButton.className = "completed";
   checkMarkContainer.appendChild(checkMarkButton);
 
-  //todo text description
-  let todoDescription = document.createElement("input");
-  todoDescription.className = "todo-content";
-  todoDescription.setAttribute("name","description-x");
-  todoDescription.setAttribute("placeholder","Type in description")
-  todoDescription.setAttribute("required","")
-  itemContainer.appendChild(todoDescription);
+  let todoDescription = document.createElement("p");
+  todoDescription.className = "todo-content-saved";
+  todoDescription.textContent = userTodoItem.description;
+  newContainer.appendChild(todoDescription);
 
-  //todo date set
-  let date =document.createElement("input");
-  date.className = "todo-date";
-  date.setAttribute("type","date");
-  date.setAttribute("name","date-x");
-  date.setAttribute("required","")
-  itemContainer.appendChild(date);
+  let date =document.createElement("p");
+  date.className = "todo-date-saved";
+  date.textContent = userTodoItem.date;
+  newContainer.appendChild(date);
 
-  //todo priority level(red, orange, yellow);
   let priority = document.createElement("div");
   priority.className = "todo-priority";
-  itemContainer.appendChild(priority);
-
-  let priorityToggle = document.createElement("input");
-  priorityToggle.setAttribute("type","button")
+  newContainer.appendChild(priority);
+  let priorityToggle = document.createElement("button");
   priorityToggle.setAttribute("name","priority-x");
   priorityToggle.setAttribute("data-priority","normal")
   priorityToggle.className = "todo-priority-set";
   priorityToggle.onclick = changePriority;
   priority.appendChild(priorityToggle);
 
-  // delete item Button
   let deleteItem = document.createElement("div");
   deleteItem.className = "todo-delete";
-  itemContainer.appendChild(deleteItem);
-
+  newContainer.appendChild(deleteItem);
   let deleteDiv = document.createElement("div");
   deleteDiv.className = "delete-icon";
   deleteItem.appendChild(deleteDiv);
-
   let deleteButton = document.createElement("input");
   deleteButton.className = "todo-remove";
+  deleteButton.onclick = cancelForm;
   deleteButton.setAttribute("type","button");
   deleteButton.setAttribute("value","+");
   deleteDiv.appendChild(deleteButton);
 
-  //NOTES text field toggle button
-  let notes = document.createElement("input");
-  notes.className = "todo-notes";
-  notes.setAttribute("type","text");
-  notes.setAttribute('name', "notes-x");
-  notes.setAttribute("placeholder","Add Notes here :)");
-  // notes.onclick = addNotes;
-  itemContainer.appendChild(notes);
+  let notesSection = document.createElement("p");
+  notesSection.className = "todo-notes-saved";
+  notesSection.textContent = `Notes: ${userTodoItem.notes}`;
+  newContainer.appendChild(notesSection);
 
-  //SAVE button
-  let saveItem = document.createElement("input");
-  saveItem.className = "todo-save";
-  saveItem.setAttribute("type","submit");
-  saveItem.setAttribute("value","Save");
-  itemContainer.appendChild(saveItem);
-
-  console.log("createItemForm has run")
-} else {alert("!!");}
-
+  // let saveButton = document.getElementsByClassName("todo-save")[0];
+  // console.log(saveButton)
+})
+const cancelForm = (()=> {
+  document.getElementById("todo-view").removeChild(document.getElementById("todo-item"));
 })
 const changePriority = (() => {
   let priorityButton = document.querySelector('[name=priority-x]');
